c3a86f69ead0337a70d5d507ad6a43002d86f5ef
anurag-singh2001 <anuragrazarwal@gmail.com>
2024-02-22 01:02:05 +0530

    Array Reduce Transformation

---
+++ Day-7.js
@@ -0,0 +1,9 @@
+var reduce = function(nums, fn, init) {
+    let res=init;
+    if(nums.length === 0)
+        return init;
+    for(let i=0;i< nums.length;i++){
+        res= fn(res,nums[i])
+    }
+    return res;
+};
